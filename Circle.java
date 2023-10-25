2dfe2f2fd2051c687d5dac2e44c33d6cc494fecf
mingtiantt <115250439+mingtiantt@users.noreply.github.com>
2023-10-25 20:17:11 +0800

    Add files via upload

---
+++ Circle.java
@@ -0,0 +1,17 @@
+package tast2;
+
+public class  Circle {
+  public double radius;
+  public final double pi=3.14;
+    public  Circle() {};
+    public Circle(double newRadius) {
+        radius = newRadius;
+    };
+    public double getArea() {
+        return pi * radius * radius;
+
+    }
+
+
+}
+
